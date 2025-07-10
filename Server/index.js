4a21421431515672d52a8fee022b9a694eefa534
FardeenKhan24 <fardeenkhan200324@gmail.com>
2025-07-10 13:36:20 +0530

    Updated cors

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 connectDB()
 
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://nex-buy-eight.vercel.app",credentials:true}))
 
 app.use("/api",authRoutes)
 app.use("/api/product",productRoutes)
