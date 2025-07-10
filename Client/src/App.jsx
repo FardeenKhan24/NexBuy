579ba6d3694f8a0ba945e1c5460504adf1889612
FardeenKhan24 <fardeenkhan200324@gmail.com>
2025-07-10 14:51:13 +0530

    Update App file

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,39 +1,19 @@
-import React, { useEffect} from 'react';
-import { useDispatch, useSelector} from 'react-redux';
-import {Routes,Route,useLocation} from 'react-router-dom';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Product from './components/Product/Product';
-import Cart from './components/Cart/Cart';
-import Footer from './components/Footer/Footer';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Profile from './components/Profile/Profile';
-import ProductDetails from './components/ProductDetails/ProductDetails.jsx';
-import BuyNow from './components/BuyNow/BuyNow.jsx';
-import About from './components/About/About.jsx';
-import Order from './components/Order/Order.jsx';
-import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes.jsx';
-import { fetchProducts } from '../src/features/productSliceApi.js';
-import { fetchCart } from '../src/features/cartSlice.js';
-import PublicRoutes from './components/PublicRoutes/PublicRoutes.jsx';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Routes, Route, useLocation } from "react-router-dom";
+import {Home,About,Login,Register,Product,ProductDetails,Cart,BuyNow,Profile,Order,Navbar,Footer,
+  ProtectedRoutes,
+  PublicRoutes,
+} from "./routes";
 
 const App = () => {
-  const dispatch = useDispatch()
   const darkMode = useSelector((state) => state.theme.darkMode);
   const location = useLocation();
-
-  const hide = location.pathname === '/login' || location.pathname === '/register';
+  const hide = location.pathname === "/login" || location.pathname === "/register";
 
   useEffect(() => {
-  document.body.className = darkMode ? 'dark-mode' : 'light-mode';
-
-  if (localStorage.getItem('token')) {
-    dispatch(fetchCart());
-  }
-
-  dispatch(fetchProducts());
-}, [dispatch, darkMode]);
+    document.body.className = darkMode ? "dark-mode" : "light-mode";
+  }, [darkMode]);
 
   return (
     <div className="App">
@@ -47,19 +27,9 @@ const App = () => {
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/products" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/buy" element={
-            <ProtectedRoutes>
-              <BuyNow />
-            </ProtectedRoutes>
-          } />
-          <Route path="/profile" element={
-            <ProtectedRoutes>
-              <Profile />
-            </ProtectedRoutes>
-          } />
-          <Route path="/order" element={
-              <Order />
-          } />
+          <Route path="/buy" element={<ProtectedRoutes><BuyNow /></ProtectedRoutes>} />
+          <Route path="/profile" element={<ProtectedRoutes><Profile /></ProtectedRoutes>} />
+          <Route path="/order" element={<Order />} />
         </Routes>
       </main>
       {!hide && <Footer />}
---
+++ Client/src/routes/index.js
@@ -0,0 +1,31 @@
+import Home from "../components/Home/Home";
+import About from "../components/About/About";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+import Product from "../components/Product/Product";
+import ProductDetails from "../components/ProductDetails/ProductDetails";
+import Cart from "../components/Cart/Cart";
+import BuyNow from "../components/BuyNow/BuyNow";
+import Profile from "../components/Profile/Profile";
+import Order from "../components/Order/Order";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import ProtectedRoutes from "../components/ProtectedRoutes/ProtectedRoutes";
+import PublicRoutes from "../components/PublicRoutes/PublicRoutes";
+
+export {
+  Home,
+  About,
+  Login,
+  Register,
+  Product,
+  ProductDetails,
+  Cart,
+  BuyNow,
+  Profile,
+  Order,
+  Navbar,
+  Footer,
+  ProtectedRoutes,
+  PublicRoutes,
+};
